5b4ca0dbdffcb5ce9b0c325264f23ff4ee78ad0b
waymes <vladimirbar7@gmail.com>
2025-04-16 08:25:19 +0300

    movie list

---
+++ frontend/src/components/movie-list-item.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+interface Movie {
+  id: number;
+  title: string;
+  description: string;
+  image: string;
+}
+const { movie } = defineProps<{
+  movie: Movie;
+}>();
+</script>
+
+<template>
+  <li class="list-group-item d-flex movie">
+    <div
+      class="image"
+      :style="{ backgroundImage: 'url(' + movie.image + ')' }"
+    />
+    <div class="ms-3">
+      <h3>{{ movie.title }}</h3>
+      <p>{{ movie.description }}</p>
+    </div>
+  </li>
+</template>
+
+<style scoped>
+.movie:hover {
+  background: #f8f9fa;
+  cursor: pointer;
+}
+.image {
+  width: 100px;
+  height: 100px;
+  background-position: center;
+  background-size: cover;
+}
+</style>
--- frontend/src/pages/home.vue
+++ frontend/src/pages/home.vue
@@ -1,5 +1,27 @@
 <script setup lang="ts">
-// import HelloWorld from './components/HelloWorld.vue'
+import MovieListItem from '../components/movie-list-item.vue';
+
+const movies = [
+  {
+    id: 1,
+    title: 'Fast and Furious',
+    description: 'A very cool action movie.',
+    image:
+      'https://www.fastxmovie.com/images/gallery/fast_x/character_driving/Vin_drive_1200x600px.jpeg',
+  },
+  {
+    id: 2,
+    title: 'Rambo',
+    description: 'Another cool action movie.',
+    image:
+      'https://upload.wikimedia.org/wikipedia/en/d/d6/First_blood_poster.jpg',
+  },
+];
 </script>
 
-<template>home</template>
+<template>
+  <h1 class="my-5 text-center">Movies list</h1>
+  <ul class="list-group">
+    <MovieListItem v-for="movie in movies" :movie="movie" />
+  </ul>
+</template>
